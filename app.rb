68301d55189ef6e811b81b43d9e62f5ed336bee6
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-29 23:40:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,17 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-get '/newteam' do
-erb :newteam
-end
+  get '/newteam' do
+    erb :newteam
+  end
 
-post '/team' do
-  @name = params["name"]
-  @coach = params["coach"]
-  @pg = params["pg"]
-  @sg = params["sg"]
-  @pf = params["pf"]
-  @sf = params["sf"]
-  @c = params["c"]
-  erb :team
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
+    erb :team
+  end
 end
